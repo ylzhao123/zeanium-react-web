89e0f69b268494b6868852b9814c44b7c21c41b6
yangyxu <945758912@qq.com>
2017-05-23 17:32:45 +0800

    update dist

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 require('./zn.react.js');
 require('./zn.react.Application.js');
 require('./util/index.js');
-
 ['global','basic','data','form','graph','loader','wap'].forEach(function (path, index){
-    zn.overwrite(zn.react, require('./view/' + path));
+    path = './view/' + path + '/index.js';
+    zn.overwrite(zn.react, require(path));
 });
 
 module.exports = window.UI = zn.react;
